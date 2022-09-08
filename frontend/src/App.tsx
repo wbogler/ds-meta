b4f8cccadc591cc27b870fec6c448902c3f79155
Willian Bogler Da Silva <willian.silva@pti.org.br>
2022-09-08 20:25:27 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
+import NotificationButton from "./components/notificationButton"
+
 function App() {
   return (
-    <h1>Olá mundo</h1>
+    <>
+      <h1>Olá mundo</h1>
+      <NotificationButton />
+    </>
   )
 }
 
