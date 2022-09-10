6290cb7d0b9092a8857da5d89197bdf3a5bc25fe
Willian Bogler Da Silva <willian.silva@pti.org.br>
2022-09-09 22:47:26 -0300

    Correção de apresentação dos nomes

--- frontend/src/components/salesCard/index.tsx
+++ frontend/src/components/salesCard/index.tsx
@@ -18,6 +18,7 @@ function SalesCard() {
     useEffect(() => {
         axios.get(`${BASE_URL}/sales`)
             .then(response => {
+                console.log(response.data)
                 setSales(response.data.content)
             })
     }, [])
@@ -63,7 +64,7 @@ function SalesCard() {
                                 <tr key = {sale.id}>
                                     <td className="show992">{sale.id}</td>
                                     <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
-                                    <td>{sale.sellerName}</td>
+                                    <td>{sale.sallerName}</td>
                                     <td className="show992">{sale.visited}</td>
                                     <td className="show992">{sale.deals}</td>
                                     <td>R$ {sale.amount.toFixed(2)}</td>
--- frontend/src/models/sale.ts
+++ frontend/src/models/sale.ts
@@ -1,8 +1,27 @@
 export type Sale ={
     id:number;
-    sellerName:string;
+    sallerName:string;
     date:string;
     visited:number;
     deals:number;
     amount:number;
 }
+
+amount
+: 
+22465
+date
+: 
+"2022-02-27"
+deals
+: 
+82
+id
+: 
+37
+sallerName
+: 
+"Padme"
+visited
+: 
+82
